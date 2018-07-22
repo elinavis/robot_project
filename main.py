fd6e86c777c0aab2cede581bd2c430f6360990be
curious <torrpolakow@gmail.com>
2018-07-22 11:21:29 +0300

    update robot matrix day 1

--- main.py
+++ main.py
@@ -2,9 +2,95 @@ from experiment_flow import *
 
 exp = ExpFlow("192.168.0.101")
 
+#############################
+####### EXP 001 Part 1 ######
+exp.initialize_exp(-1, 0, 1)
+exp.start(0)
 
-exp.initialize_exp(-1, 1, 0)
-exp.start(1)
+####### EXP 001 Part 2 ######
+# exp.initialize_exp(0, 1, -1)
+# exp.start(1)
+#############################
+
+#############################
+####### EXP 002 Part 1 ######
+# exp.initialize_exp(0, 1, -1)
+# exp.start(0)
+#
+####### EXP 002 Part 2 ######
+# exp.initialize_exp(1, -1, 0)
+# exp.start(1)
+#############################
+
+#############################
+####### EXP 003 Part 1 ######
+# exp.initialize_exp(1, -1, 0)
+# exp.start(0)
+#
+####### EXP 003 Part 2 ######
+# exp.initialize_exp(0, 1, -1)
+# exp.start(1)
+#############################
+
+#############################
+####### EXP 004 Part 1 ######
+# exp.initialize_exp(0, 1, -1)
+# exp.start(0)
+
+####### EXP 004 Part 2 ######
+# exp.initialize_exp(-1, 0, 1)
+# exp.start(1)
+#############################
+
+#############################
+####### EXP 005 Part 1 ######
+# exp.initialize_exp(1, -1, 0)
+# exp.start(0)
+
+####### EXP 005 Part 2 ######
+# exp.initialize_exp(-1, 0, 1)
+# exp.start(1)
+#############################
+
+#############################
+####### EXP 006 Part 1 ######
+# exp.initialize_exp(-1, 0, 1)
+# exp.start(0)
+
+####### EXP 006 Part 2 ######
+# exp.initialize_exp(1, -1, 0)
+# exp.start(1)
+#############################
+
+#############################
+####### EXP 007 Part 1 ######
+# exp.initialize_exp(1, -1, 0)
+# exp.start(0)
+
+####### EXP 007 Part 2 ######
+# exp.initialize_exp(0, 1, -1)
+# exp.start(1)
+#############################
+
+#############################
+####### EXP 008 Part 1 ######
+# exp.initialize_exp(-1, 0, 1)
+# exp.start(0)
+
+####### EXP 008 Part 2 ######
+# exp.initialize_exp(1, -1, 0)
+# exp.start(1)
+#############################
+
+#############################
+####### EXP 009 Part 1 ######
+# exp.initialize_exp(0, 1, -1)
+# exp.start(0)
+
+####### EXP 009 Part 2 ######
+# exp.initialize_exp(-1, 0, 1)
+# exp.start(1)
+#############################
 
 
 
